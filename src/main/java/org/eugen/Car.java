9805e6254f5d48626fec11a0b2f49fccfc6d619d
Eugen Stefanescu <eugen2994@gmail.com>
2021-01-12 18:56:57 +0200

    added Car class

---
+++ src/main/java/org/eugen/Car.java
@@ -0,0 +1,15 @@
+package org.eugen;
+
+public class Car {
+
+    //instance variables
+    String name;
+    double fuelLevel;
+    double mileage;
+    double totalTraveledDistance;
+    double maxSpeed;
+
+    int doorCount;
+    boolean damaged;
+    String color;
+}
