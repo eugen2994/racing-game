9e02db554715e527aac3720c929afe454b69956d
Eugen Stefanescu <eugen2994@gmail.com>
2021-01-14 23:07:11 +0200

    declaring methods

--- src/main/java/org/eugen/Vehicle.java
+++ src/main/java/org/eugen/Vehicle.java
@@ -12,4 +12,26 @@ public class Vehicle {
 
     boolean damaged;
     String color;
+
+    public double accelerate(double speed, double durationInHours) {
+        System.out.println(name + "is accelerationg with " + speed + "km/h for " + durationInHours + "h");
+
+        //local variable
+        double distance = speed * durationInHours;
+
+        totalTraveledDistance = totalTraveledDistance + distance;
+
+        //same results as the statement above
+        //totalTraveledDistance += distance;
+
+        System.out.println("Total traveled distance for vehicle " + name + ": " + totalTraveledDistance);
+
+        double usedFuel = distance + mileage/100;
+
+        fuelLevel -= usedFuel;
+        System.out.println("remaining fuel for vehicle " + name + ": " + fuelLevel);
+
+        return distance;
+    }
+
 }
